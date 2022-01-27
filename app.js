0132920763807ae368ca776d1a361fd4d896a086
Hatem Soliman <hatemthedev@gmail.com>
2022-01-27 23:34:27 +0200

    JS Tweaks

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //VARIABLES
 const targets = document.querySelectorAll('img');
+const form = document.getElementById('contact__form');
+const firstName = document.getElementById('first__name');
+const lastName = document.getElementById('last__name');
+const email = document.getElementById('email');
+const submitButton = document.getElementById('form__btn');
+const error = document.getElementById('error__msg');
 
 //FUNCTIONS
 
@@ -29,3 +35,24 @@ targets.forEach(lazyLoad);
 
 
 //Form Validation
+// submitButton.addEventListener('submit', (e) => {
+//   let messages = []
+//   if(firstName.value === '' || firstName.value == null) {
+//     errorMessage.push('Name is required! Please enter a name.')
+//     const errorMessage = document.createElement('h5');
+//     errorMessage.innerText = `Name is required! Please enter a name.`;
+//     errorMessage.classList.add('error');
+//     error.appendChild(errorMessage);
+//   }
+
+//   if(lastName.value === '' || lastName.value == null) {
+//     messages.push('Name is required! Please enter a name.')
+//   }
+// })
+
+
+//Booking & Planning
+submitButton.addEventListener('submit', (e) => {
+  
+})
+
